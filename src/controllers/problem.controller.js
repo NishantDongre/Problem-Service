a78180834871236de7ef6ed9b5ef1977108d74bc
Nishant <nishantdongre30@gmail.com>
2024-04-05 16:23:40 +0530

    Feat: Added routing structure for routing API for Problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,22 @@
+function pingProblemController(req, res) {
+  res.json({ message: "Problem Controller is up!" });
+}
+
+function addProblem(req, res) {}
+
+function getProblem(req, res) {}
+
+function getProblems(req, res) {}
+
+function deleteProblem(req, res) {}
+
+function updateProblem(req, res) {}
+
+module.exports = {
+  addProblem,
+  getProblem,
+  getProblems,
+  deleteProblem,
+  updateProblem,
+  pingProblemController,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/server.config");
+const apiRouter = require("./routes");
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,7 @@ app.get("/ping", (req, res) => {
   return res.json({ message: "Problem Service is alive" });
 });
 
+app.use("/api", apiRouter);
 app.listen(PORT, () => {
   console.log(`Problem-Server listenning on ${PORT}`);
 });

53767cc28635124f97f060095852e9fdd11c793c
Nishant <nishantdongre30@gmail.com>
2024-04-08 22:54:30 +0530

    Feat: Completed Create-Problem-API

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,14 +1,26 @@
 const { StatusCodes } = require("http-status-codes");
 const NotImplemented = require("../errors/notImplemented.error");
+const { ProblemService } = require("../services");
+const { ProblemRepository } = require("../repositories");
+
+const problemService = new ProblemService(new ProblemRepository());
+
 function pingProblemController(req, res) {
   return res
     .status(StatusCodes.OK)
     .json({ message: "Problem Server is up and running" });
 }
 
-function addProblem(req, res, next) {
+async function addProblem(req, res, next) {
   try {
-    throw new NotImplemented("Add Problem");
+    console.log("incoming req body", req.body);
+    const newproblem = await problemService.createProblem(req.body);
+    return res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "Successfully created a new problem",
+      error: {},
+      data: newproblem,
+    });
   } catch (error) {
     next(error);
   }
